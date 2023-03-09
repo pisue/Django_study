009024545e9155df906653dfd41656a14670851a
pisue <pisue22@naver.com>
2023-03-09 17:14:44 +0900

    3-07 마무리

--- pybo/models.py
+++ pybo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -8,9 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.subject
 
+
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models. TextField()
+    content = models.TextField()
     create_date = models.DateTimeField()
-
-
